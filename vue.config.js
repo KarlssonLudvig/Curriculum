3d2144e8c4e0560d7f7b75a079ea921cc25e23fd
KarlssonLudvig <Ludvig.kh.karlsson@gmail.com>
2020-01-30 09:31:00 +0100

    config change

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify",
   ],
-<<<<<<< HEAD
   publicPath: process.env.NODE_ENV === 'production'
   ? '/Curriculum/'
   : '/'
-=======
-  publicPath: "Curriculum"
->>>>>>> dist folder
 }
